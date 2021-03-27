4866c58d3e1130b9ab77ddbca82327e3e6086b24
dradoslavov01 <daniradoslavov01@abv.bg>
2021-03-27 18:21:12 +0200

    Created static moveies sections example

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
-import * as api from './api/data.js';
+import page from '//unpkg.com/page/page.mjs';
+import { html, render } from '//unpkg.com/lit-html?module';
 
-window.api = api;
+const options = {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "b9d3085a18msh5adb9f4f8ebc979p193186jsna16d9c4f8ee5",
+		"x-rapidapi-host": "imdb8.p.rapidapi.com"
+	}};
+
+const movieName = encodeURI('q=fast');
+const response = await fetch("https://imdb8.p.rapidapi.com/auto-complete?" + movieName, options);
+const data = await response.json();
+console.log(data.d);
+
+
